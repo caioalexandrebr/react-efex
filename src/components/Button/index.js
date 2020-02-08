2b2ccabe2469da53256a622104090bc2659e39b1
Caio Alexandre <caioalexandre.br@gmail.com>
2020-02-08 12:14:28 -0300

    refactor(Button): refactoring button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const ButtonDefault = styled.button`
-  color: red;
+  color: #009748;
   font-size: 16px;
   padding: 15px 50px;
-  border: 3px solid red;
+  border: 3px solid #009748;
   border-radius: 9999px;
   background: transparent;
   text-transform: uppercase;
