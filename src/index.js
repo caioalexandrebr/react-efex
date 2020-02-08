314e35435b90a1a23a1f40cd795799829b42749d
Caio Alexandre <caioalexandre.br@gmail.com>
2020-02-08 12:16:59 -0300

    refactor(pages): modularizing pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './Pages/Home';
+import Home from './pages/Home';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
