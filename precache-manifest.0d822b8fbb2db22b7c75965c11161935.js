8606558b9a573e2e314847d4fa64b019b9f7430a
Caio Alexandre <caioalexandre.br@gmail.com>
2020-02-08 11:01:51 -0300

    Updates

---
+++ precache-manifest.0d822b8fbb2db22b7c75965c11161935.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7565a8a4e2982d1220fa14e1508d586b",
+    "url": "/react-efex/index.html"
+  },
+  {
+    "revision": "acbd672b50d131049ecb",
+    "url": "/react-efex/static/css/main.29d216ee.chunk.css"
+  },
+  {
+    "revision": "28cc87b1f31492f21bc5",
+    "url": "/react-efex/static/js/2.4b9bcfb5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-efex/static/js/2.4b9bcfb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acbd672b50d131049ecb",
+    "url": "/react-efex/static/js/main.adddd4fc.chunk.js"
+  },
+  {
+    "revision": "e120d5eaa1849cf91050",
+    "url": "/react-efex/static/js/runtime-main.98d927dc.js"
+  },
+  {
+    "revision": "0c33d76a7bf9226b0e924ba4cebd0cab",
+    "url": "/react-efex/static/media/bg-soldier.0c33d76a.png"
+  }
+]);
