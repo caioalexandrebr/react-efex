87b9d3c938a1082e969e0c177902fbb3523d8ff9
Caio Alexandre <caioalexandre.br@gmail.com>
2020-02-09 11:04:51 -0300

    style(Device): adding breakpoint system

---
+++ src/components/Device/index.js
@@ -0,0 +1,19 @@
+const size = {
+  xs: '360px',
+  sm: '480px',
+  md: '768px',
+  lg:	'1024px',
+  xl: '1200px',
+  xxl: '1366px',
+  xxxl: '1600px'
+}
+
+export const device = {
+  xs: `(min-width: ${size.xs})`,
+  sm: `(min-width: ${size.sm})`,
+  md: `(min-width: ${size.md})`,
+  lg: `(min-width: ${size.lg})`,
+  xl: `(min-width: ${size.xl})`,
+  xxl: `(min-width: ${size.xxl})`,
+  xxxl: `(min-width: ${size.xxxl})`
+};
