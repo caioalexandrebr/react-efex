f6484f6db24db08bea42880d3c849bed432db5b9
Caio Alexandre <caioalexandre.br@gmail.com>
2020-02-10 13:39:01 -0300

    style(Button): styling with transition

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -5,11 +5,17 @@ const ButtonDefault = styled.button`
   color: #009748;
   font-size: 16px;
   padding: 15px 50px;
-  border: 3px solid #009748;
+  transition: all 0.1s;
   border-radius: 9999px;
   background: transparent;
+  border: 3px solid #009748;
   text-transform: uppercase;
   font-family: 'Oswald', sans-serif;
+
+  &:hover {
+    color: white;
+    background: #009748;
+  }
 `;
 
 const Button = ({ children, ...props }) => <ButtonDefault {...props} >{children}</ButtonDefault>;
