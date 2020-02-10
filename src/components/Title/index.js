3bfa747b39b22be3e2462e377ebd4f8eb782ee8a
Caio Alexandre <caioalexandre.br@gmail.com>
2020-02-10 09:09:41 -0300

    refactor(Title): adding inverted prop and refactoring

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,23 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 const Text = styled.h3`
   font-size: 40px;
   font-weight: 100;
   text-transform: uppercase;
-  color: ${props => props.primary ? "white" : "black"};
+  color: ${props => props.inverted ? "white" : "black"};
 
   span {
     color: #009748;
   }
 `;
 
-class Title extends Component {
+/* class Title extends Component {
   render() {
     return (
-      <Text>{this.props.children}</Text>
+      <Text {...this.props}>{this.props.children}</Text>
     );
   }
-}
+} */
+
+const Title = ({ children, ...props }) => <Text {...props} >{children}</Text>;
 
 export default Title;
